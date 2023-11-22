17796ad54dce7d8e8fbc183a137fe40c594f9842
Pratibhapa <pratibhaa4321@gmail.com>
2023-11-22 13:38:32 +0545

    updating tictac board

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useStaten, useRef, useState} from 'react';
 import './TicTacToe.css';
 
 import bluecirle from '../Assets/bluecircle.jpg'
@@ -9,11 +9,13 @@ let data = ["", "", "", "", "", "", "", "", ""];
 const TicTacToe = () => {
     let [count, setCount] = useState(0);
     let [lock, setLock] = useState(false);
+    let titleRef = useRef(null);
 
     const toggle = (e, num) => {
-        if (lock) {
+        if (lock || data[num] !== "") {
             return 0;
         }
+
         if (count % 2 === 0) {
             e.target.innerHTML = `<img src='${bluecirle}'>`;
             data [num] = "x";
@@ -32,65 +34,73 @@ const TicTacToe = () => {
     const checkwin = () => {
         if (data[0]===data[1] && data[1]===data[2] && data[2]!=="")
         {
-            won(data);
+            won(data[2]);
         }
         else if(data[3]===data[4] && data[4]===data[5] && data[5]!=="")
         {
-            won(data);
+            won(data[5]);
         }
         else if (data[6]===data[7] && data[7]===data[8] && data[8]!=="")
         {
-            won(data);
+            won(data[8]);
         }
         else  if (data[0]===data[3] && data[3]===data[6] && data[6]!=="")
         {
-            won(data);
+            won(data[6]);
         }
         else  if (data[1]===data[4] && data[4]===data[7] && data[7]!=="")
         {
-            won(data);
+            won(data[7]);
         }
         else  if (data[2]===data[5] && data[5]===data[8] && data[8]!=="")
         {
-            won(data);
+            won(data[8]);
         }
         else  if (data[0]===data[4] && data[4]===data[8] && data[8]!=="")
         {
-            won(data);
+            won(data[8]);
         }
         else  if (data[0]===data[1] && data[1]===data[2] && data[2]!=="")
         {
-            won(data);
+            won(data[2]);
         }
         else  if (data[2]===data[4] && data[4]===data[6] && data[6]!=="")
         {
-            won(data);
+            won(data[6]);
         }
     };
 
 
     const won = (winner) => {
         setLock(true);
+        if(winner==="x")
+        {
+            titleRef.current.innerHTML = `Congratulations: <img src=${bluecirle}>`;
+        }
+        else
+        {
+            titleRef.current.innerHTML = `Congratulations: <img src=${yellow}>`;
+        }
     }
 
     return (
         <div className='container'>
-            <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
+            <h1 className='title' ref={titleRef}>Tic Tac Toe Game In <span>React</span></h1>
             <div className='board'>
                 <div className='row1'>
+                    <div className='boxes' onClick={(e) => toggle(e, 0)}></div>
                     <div className='boxes' onClick={(e) => toggle(e, 1)}></div>
                     <div className='boxes' onClick={(e) => toggle(e, 2)}></div>
-                    <div className='boxes' onClick={(e) => toggle(e, 3)}></div>
                 </div>
                 <div className='row2'>
+                    <div className='boxes' onClick={(e) => toggle(e, 3)}></div>
                     <div className='boxes' onClick={(e) => toggle(e, 4)}></div>
-                    <div className='boxes' onClick={(e) => toggle(e, 5)}></div>
-                    <div className='boxes' onClick={(e) => toggle(e,  6)}></div>
+                    <div className='boxes' onClick={(e) => toggle(e,  5)}></div>
                 </div>
                 <div className='row3'>
+                    <div className='boxes' onClick={(e) => toggle(e,  6)}></div>
                     <div className='boxes' onClick={(e) => toggle(e,  7)}></div>
                     <div className='boxes' onClick={(e) => toggle(e,  8)}></div>
-                    <div className='boxes' onClick={(e) => toggle(e,  9)}></div>
                 </div>
             </div>
             <button className='reset'>Reset</button>
