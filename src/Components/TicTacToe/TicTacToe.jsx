d17c4879d7e72cf2246536cbbfe3785b37015f6b
Pratibhapa <pratibhaa4321@gmail.com>
2023-11-22 10:06:56 +0545

    file added

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TicTacToe.css';
+
+import bluecirle from '../Assets/bluecircle.jpg'
+import yellow from  '../Assets/yellow.png'
+const TicTacToe = () => {
+  return (
+    <div>
+      
+      
+    </div>
+  )
+}
+
+export default TicTacToe
